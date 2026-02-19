10423c9bd5fd61ebf2a6429e6f6a17924675b54f
206sahilsingh <206sahilsingh@gmail.com>
2026-02-20 00:27:53 +0530

    Time: 2 ms (78.20%) | Memory: 7.8 MB (36.30%) - LeetSync

---
+++ 1013-fibonacci-number/fibonacci-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int fib(int n) {
+        int a=0,b=1,c;
+       if(n==0){
+        return a;
+       } 
+       else if(n==1){
+        return b;
+       }
+       else{
+        for(int i=0; i<=n-2;i++){
+        c=a+b;
+        a=b;
+        b=c;
+        }
+         return b;
+       }
+    }
+};

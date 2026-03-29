31fd39bd58e5ce9d7c8eae38bf0cff6645bd3218
206sahilsingh <206sahilsingh@gmail.com>
2026-03-29 15:22:21 +0530

    Time: 0 ms (100.00%) | Memory: 15.8 MB (60.61%) - LeetSync

--- 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
+++ 2283-sort-even-and-odd-indices-independently/sort-even-and-odd-indices-independently.cpp
@@ -20,9 +20,11 @@ public:
         int j = 0, k = 0;
         for(int i = 0; i < nums.size(); i++) {
             if(i % 2 == 0) {
-                nums[i] = even[j++];
+                nums[i] = even[j];
+                j++;
             } else {
-                nums[i] = odd[k++];
+                nums[i] = odd[k];
+                k++;
             }
         }
 

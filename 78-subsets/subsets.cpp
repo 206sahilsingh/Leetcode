1d375269b72230b3265f24bf3764384ebf132889
206sahilsingh <206sahilsingh@gmail.com>
2026-04-08 01:58:15 +0530

    Time: 3 ms (28.39%) | Memory: 16.9 MB (5.39%) - LeetSync

---
+++ 78-subsets/subsets.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+
+    void subset(vector<int>nums,int index, int n,vector<vector<int>>&ans, vector<int>temp){
+
+    if(index==n){
+        ans.push_back(temp);
+        return;
+    }
+    //not include
+    subset(nums,index+1,n,ans,temp);
+  
+    //include
+    temp.push_back(nums[index]);
+    subset(nums,index+1,n,ans,temp); 
+
+    }
+
+    vector<vector<int>> subsets(vector<int>& nums) {
+       vector<vector<int>>ans;
+       vector<int>temp;
+       subset(nums,0,nums.size(),ans,temp); 
+       return ans;
+    }
+};

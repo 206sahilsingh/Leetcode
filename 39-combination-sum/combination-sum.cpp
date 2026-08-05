affbff408b41a06605c6928b2928af3526f45a24
206sahilsingh <206sahilsingh@gmail.com>
2026-08-06 02:51:28 +0530

    Time: 0 ms (100.00%) | Memory: 14.1 MB (59.82%) - LeetSync

---
+++ 39-combination-sum/combination-sum.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void Sum(int index,vector<int>&candidates,int target,vector<vector<int>>& ans, vector<int>& curr){
+        if(target ==0){
+        ans.push_back(curr);    
+        return;}
+
+        if(index== candidates.size() ||target<0)
+        return;
+
+        curr.push_back(candidates[index]);
+        Sum(index,candidates,target-candidates[index],ans,curr);
+        curr.pop_back();
+
+        Sum(index+1,candidates,target,ans,curr);
+    }
+
+    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+        vector<vector<int>>ans;
+        vector<int>curr;
+
+        Sum(0,candidates,target,ans,curr);
+        return ans;
+    }
+};

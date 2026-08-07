91e0a3f2da7b7cb9bc96d94c762f596f214fb677
206sahilsingh <206sahilsingh@gmail.com>
2026-08-08 01:12:47 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (11.27%) - LeetSync

---
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int hammingWeight(int n) {
+        string ans=bitset<32>(n).to_string();
+
+        int count=0;
+        for(int i=0;i<ans.size();i++){
+            if(ans[i]=='1')
+            count++;
+        }
+        return count;
+    }
+};
